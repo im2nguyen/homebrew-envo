fb575b1f9e66db7edac51606df80b355a9ed3b76
Tu Nguyen <im2nguyen@gmail.com>
2021-09-02 01:41:49 -0700

    Update SHA

--- envo.rb
+++ envo.rb
@@ -2,7 +2,7 @@ class Envo < Formula
   desc "A CLI tool that masks and obscurates your environment variables for demos"
   homepage "https://github.com/im2nguyen/envo"
   url "https://github.com/im2nguyen/envo/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "5b2243e4278e895fe79a4939f1acc5baee502ea6b309d7df39e7aac515e7d0cf"
+  sha256 "bc48807e5c42897152469336f783e2e7471f731f03719c75549c55695e18b6a9"
   license "MIT"
 
   # depends_on "cmake" => :build
